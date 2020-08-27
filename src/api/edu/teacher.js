3ce533b02dc06603dce4c4edab83ee2ea8c754a3
gyyHappy <244160428@qq.com>
2020-08-27 11:43:10 +0800

    第一次提交

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,19 @@
+import request from '@/utils/request'
+
+export default {
+    //1、分页条件查询讲师列表
+    getTeacherListPage(current,limit,teacherQuery){
+        return request ({
+            url: `/eduservice/teacher/pageListCondition/${current}/${limit}`,
+            method: 'post',
+            data: teacherQuery
+        })
+    },
+    //删除讲师
+    deleteTeacherId(id){
+        return request ({
+            url: `/eduservice/teacher/${id}`,
+            method: 'delete'
+        })
+    }
+}
